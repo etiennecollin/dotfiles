67200ba7acc6a78db1fd2ee77a1ac9d8ee778b67
Etienne Collin <99756528+etiennecollin@users.noreply.github.com>
2023-05-11 03:23:24 -0400

    Updated script removing sudo for power commands

--- install/other.sh
+++ install/other.sh
@@ -34,20 +34,42 @@ done
 ########################################################################################################################
 # Sudo without password for power commands (systemctl poweroff, halt, reboot, suspend and hibernate)
 ########################################################################################################################
+commentString="# Disabling sudo for power commands."
+commandSring="${commentString}\n$USER archlinux =NOPASSWD: /usr/bin/systemctl poweroff, /usr/bin/systemctl halt, /usr/bin/systemctl reboot, /usr/bin/systemctl suspend, /usr/bin/systemctl hibernate\n\n"
+
+# Evaluate command to remove ${commentString} and $USER
+eval $commandString >/dev/null
 
 while true; do
-    echo "\nDo you require sudo for power commands (systemctl poweroff, halt, reboot, suspend and hibernate)?"
+    echo ""
+    echo "Do you require sudo for power commands (systemctl poweroff, halt, reboot, suspend and hibernate)?"
     echo "1. Yes"
     echo "2. No"
     printf "Input: "
     read input
 
     if [ "$input" = "1" ]; then
-        echo "Sudo will be required for power commands"
+        # Check if command is already in sudoers file
+        sudo grep -Pzq "${commandSring}" /etc/sudoers >/dev/null
+        if [ $? == 0 ]; then
+            echo "Enabling sudo for power commands..."
+            # Using | as a delimiter because the command string contains /
+            sudo sed -i "/^${commentString}$/,/^$/d" /etc/sudoers
+        else
+            echo "Sudo already enabled for power commands. Skipping..."
+        fi
+
         break
     elif [ "$input" = "2" ]; then
-        echo "Disabling sudo for power commands..."
-        printf "\n$USER archlinux =NOPASSWD: /usr/bin/systemctl poweroff,/usr/bin/systemctl halt,/usr/bin/systemctl reboot, /usr/bin/systemctl suspend, /usr/bin/systemctl hibernate\n" | sudo tee -a /etc/sudoers
+        # Check if command is already in sudoers file
+        sudo grep -Pzq "${commandSring}" /etc/sudoers >/dev/null
+        if [ $? == 0 ]; then
+            echo "Sudo already disabled for power commands. Skipping..."
+        else
+            echo "Disabling sudo for power commands..."
+            printf "${commandSring}" | sudo tee -a /etc/sudoers >/dev/null
+        fi
+
         break
     else
         echo "Wrong input"
