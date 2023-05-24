02ef0d1815a7968974c3b446107f474d2f4afaab
Etienne Collin <collin.etienne.contact@gmail.com>
2023-05-23 20:01:25 -0400

    Merged scripts

--- scripts/other.sh
+++ scripts/other.sh
@@ -98,4 +98,41 @@ if [ -x $(command -v nvidia-smi) ]; then
     sudo mkinitcpio -P >/dev/null
 fi
 
+########################################################################################################################
+# Prepare NeoVim
+########################################################################################################################
+
+echo "Installing neovim in python..."
+pip3 install neovim
+
+# Install packer.nvim
+echo "Installing packer.nvim..."
+if [ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim >/dev/null
+else
+    echo "packer.nvim already installed. Skipping..."
+fi
+
+########################################################################################################################
+# Oh-my-zsh
+########################################################################################################################
+
+echo "Setting up oh-my-zsh..."
+if [ ! -x $(command -v omz) ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "Oh-my-zsh already installed. Skipping..."
+fi
+
+########################################################################################################################
+# Powerlevel10k
+########################################################################################################################
+
+echo "Setting up Powerlevel10k..."
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k >/dev/null
+else
+    echo "Powerlevel10k already installed. Skipping..."
+fi
+
 exit 0
