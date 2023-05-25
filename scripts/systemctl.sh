697d27f60e123eddc3fb5715634203a32479871e
Etienne Collin <99756528+etiennecollin@users.noreply.github.com>
2023-05-24 21:00:55 -0400

    Updated yubico package

--- scripts/systemctl.sh
+++ scripts/systemctl.sh
@@ -8,6 +8,9 @@ sudo systemctl enable sshd
 echo "Enabling fail2ban..."
 sudo systemctl enable fail2ban
 
+echo "Enabling pcscd for yubikey management..."
+sudo systemctl enable pcscd.service
+
 if [ -x "$(command -v betterlockscreen)" ]; then
     echo "Enabling betterlockscreen..."
     sudo systemctl enable betterlockscreen@$USER
