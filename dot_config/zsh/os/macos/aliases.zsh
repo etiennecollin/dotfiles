a1fccc759b7f7401088158a231d087210b5235ed
etiennecollin <collin.etienne.contact@gmail.com>
2024-05-27 20:22:15 -0400

    Updated WOL port

--- dot_config/zsh/os/macos/aliases.zsh
+++ dot_config/zsh/os/macos/aliases.zsh
@@ -1,1 +1,1 @@
-alias wol="wakeonlan -p 22022 -i $(dig +short server.etiennecollin.com) 24:4b:fe:8e:2f:9c"
+alias wol="wakeonlan -p 22021 -i $(dig +short server.etiennecollin.com) 24:4b:fe:8e:2f:9c"
