b8ed89781affdb5e400ea70699b346090be5ae16
etiennecollin <collin.etienne.contact@gmail.com>
2024-05-30 15:49:18 -0400

    Added yazi

--- install-scripts/arch.sh
+++ install-scripts/arch.sh
@@ -5,7 +5,7 @@ if ! command -v yay >/dev/null; then
 	sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay
 fi
 
-yay -Syuq --needed bat blender bottom-git brave-bin brave-browser calf calibre celluloid discord discord easyeffects ethtool fail2ban fastfetch freecad fzf gamemode github-cli gnupg graphviz hakuneko-desktop-bin hexyl htop kicad lazygit libreoffice-fresh lsp-plugins man-db man-pages neovim noto-fonts ntfs-3g nvtop openssh openssl os-prober pandoc-cli parallel poetry python ripgrep rustup sed silicon sioyek steam stlink sudo testdisk tlrc tmux transmission-gtk trash tree ttf-jetbrains-mono-nerd typst udiskie ufw unzip via-bin wezterm wget xclip xdg-ninja-git xdg-user-dirs xdg-utils yubico-authenticator-bin yubikey-manager-qt zellij zip zoxide zsh
+yay -Syuq --needed bat blender bottom-git brave-bin brave-browser calf calibre celluloid discord discord easyeffects ethtool fail2ban fastfetch fd ffmpegthumbnailer freecad fzf gamemode github-cli gnupg graphviz hakuneko-desktop-bin hexyl htop jq kicad lazygit libreoffice-fresh lsp-plugins man-db man-pages neovim noto-fonts ntfs-3g nvtop openssh openssl os-prober pandoc-cli parallel poetry poppler python ripgrep rustup sed silicon sioyek steam stlink sudo testdisk tlrc tmux transmission-gtk trash tree ttf-jetbrains-mono-nerd typst udiskie ufw unarchiver unzip via-bin wezterm wget xclip xdg-ninja-git xdg-user-dirs xdg-utils yazi yubico-authenticator-bin yubikey-manager-qt zellij zip zoxide zsh
 
 # Other dependencies
 # texlive-meta
