baae80207964020f333ae47a45b7038967ae093b
etiennecollin <collin.etienne.contact@gmail.com>
2024-06-28 15:39:01 -0400

    Installing neovim python dependencies

--- install-scripts/macos.sh
+++ install-scripts/macos.sh
@@ -6,12 +6,15 @@ if ! command -v brew >/dev/null; then
 fi
 
 # Dependencies of dotfiles
-brew install --quiet --formula bat eza fastfetch fd ffmpegthumbnailer fzf gh git-delta gnu-sed gnupg graphviz hexyl jq lazygit neovim oh-my-posh ollama openssl pandoc parallel pngpaste poetry poppler python ripgrep silicon stlink testdisk tlrc tmux trash tree typst unar wakeonlan wget yazi zellij zoxide
+brew install --quiet --formula bat eza fastfetch fd ffmpegthumbnailer fzf gh git-delta gnu-sed gnupg graphviz hexyl jq lazygit neovim oh-my-posh ollama openssl pandoc parallel pngpaste poetry poppler python ripgrep silicon testdisk tlrc tmux trash tree typst unar wakeonlan wget yazi zellij zoxide
 
 brew install --quiet --cask aldente appcleaner blender brave-browser calibre coconutbattery discord font-jetbrains-mono-nerd-font freecad glance-chamburr hakuneko keepassxc kicad libreoffice macs-fan-control omnidisksweeper rectangle sioyek transmission wezterm
 
 # Other dependencies
 # mactex ghdl gtkwave
+
+# Install python packages
+pip install --break-system-packages neovim
 
 # Make sure rust is installed
 if ! command -v rustup >/dev/null; then
