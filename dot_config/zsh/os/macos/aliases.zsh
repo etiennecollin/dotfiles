a9e222680130e47bcd85a46a8cce909e0339a134
etiennecollin <collin.etienne.contact@gmail.com>
2024-08-03 16:24:58 -0400

    Updated wol port

--- dot_config/zsh/os/macos/aliases.zsh
+++ dot_config/zsh/os/macos/aliases.zsh
@@ -1,1 +1,1 @@
-alias wol="curl --request PUT \"http://server.etiennecollin.com:8080/wol?mac_addr=24:4b:fe:8e:2f:9c\" >/dev/null"
+alias wol="curl --request PUT \"http://server.etiennecollin.com/wol?mac_addr=24:4b:fe:8e:2f:9c\" >/dev/null"
