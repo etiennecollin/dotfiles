bfd0f81d4e238c18d46b8b3204c6823ed59dde90
etiennecollin <collin.etienne.contact@gmail.com>
2024-08-23 22:12:26 -0400

    Updated subdomain

--- dot_config/zsh/os/macos/aliases.zsh
+++ dot_config/zsh/os/macos/aliases.zsh
@@ -1,1 +1,1 @@
-alias wol="curl --request PUT \"http://wol.lan.etiennecollin.com/wol?mac_addr=24:4b:fe:8e:2f:9c\" >/dev/null"
+alias wol="curl --request PUT \"http://wol.home.etiennecollin.com/wol?mac_addr=24:4b:fe:8e:2f:9c\" >/dev/null"
