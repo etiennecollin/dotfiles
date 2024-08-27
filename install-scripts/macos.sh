d2c779a90d9081d6be87301e87db5044e6872d8b
etiennecollin <collin.etienne.contact@gmail.com>
2024-08-26 22:34:01 -0400

    Added lazygit

--- install-scripts/macos.sh
+++ install-scripts/macos.sh
@@ -2,11 +2,11 @@
 
 # Make sure brew is installed
 if ! command -v brew >/dev/null; then
-	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Dependencies of dotfiles
-brew install --quiet --formula bat eza fastfetch fd ffmpegthumbnailer fzf gh git-delta gnu-sed gnupg graphviz hexyl jq lazygit neovim oh-my-posh ollama openssl pandoc parallel pngpaste poetry poppler python ripgrep silicon testdisk tlrc tmux trash tree typst unar wakeonlan wget yazi zellij zoxide
+brew install --quiet --formula bat eza fastfetch fd ffmpegthumbnailer fzf gh git-delta gnu-sed gnupg graphviz hexyl jq lazydocker lazygit neovim oh-my-posh ollama openssl pandoc parallel pngpaste poetry poppler python ripgrep silicon testdisk tlrc tmux trash tree typst unar wakeonlan wget yazi zellij zoxide
 
 brew install --quiet --cask aldente appcleaner blender brave-browser calibre coconutbattery discord font-jetbrains-mono-nerd-font freecad glance-chamburr hakuneko keepassxc kicad libreoffice macs-fan-control omnidisksweeper rectangle sioyek transmission wezterm
 
@@ -15,13 +15,13 @@ brew install --quiet --cask aldente appcleaner blender brave-browser calibre coc
 
 # Make sure rust is installed
 if ! command -v rustup >/dev/null; then
-	echo "| ########################################"
-	echo "| Installing rust..."
-	echo "| ########################################"
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    echo "| ########################################"
+    echo "| Installing rust..."
+    echo "| ########################################"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 else
-	echo "| ########################################"
-	echo "| Updating rust..."
-	echo "| ########################################"
-	rustup update
+    echo "| ########################################"
+    echo "| Updating rust..."
+    echo "| ########################################"
+    rustup update
 fi
