28c907246e027b84b4d56dc1ac875841fc45fc7b
etiennecollin <collin.etienne.contact@gmail.com>
2024-09-14 17:12:36 -0400

    Formatting

--- dot_config/zsh/functions.zsh
+++ dot_config/zsh/functions.zsh
@@ -1,162 +1,230 @@
 function secret { # list preferred id last
-	output="$(dirname ${1})/$(basename ${1}).$(date +%F).asc"
-	recipients=${2:-collin.etienne.contact@gmail.com}
+    output="$(dirname ${1})/$(basename ${1}).$(date +%F).asc"
+    recipients=${2:-collin.etienne.contact@gmail.com}
 
-	gpg --encrypt --armor --output ${output} -r "${recipients}" "${1}" &&
-		echo "$(basename "${1}") -> $(basename "${1}").$(date +%F).asc"
+    gpg --encrypt --armor --output ${output} -r "${recipients}" "${1}" &&
+        echo "$(basename "${1}") -> $(basename "${1}").$(date +%F).asc"
 }
 
 function secret-sign { # list preferred id last
-	output="$(dirname ${1})/$(basename ${1}).$(date +%F).asc"
-	recipients=${2:-collin.etienne.contact@gmail.com}
+    output="$(dirname ${1})/$(basename ${1}).$(date +%F).asc"
+    recipients=${2:-collin.etienne.contact@gmail.com}
 
-	gpg --sign --encrypt --armor --output ${output} -r "${recipients}" "${1}" &&
-		echo "$(basename "${1}") -> $(basename "${1}").$(date +%F).asc"
+    gpg --sign --encrypt --armor --output ${output} -r "${recipients}" "${1}" &&
+        echo "$(basename "${1}") -> $(basename "${1}").$(date +%F).asc"
 }
 
 function reveal {
-	output=$(echo "${1}" | rev | cut -c16- | rev)
+    output=$(echo "${1}" | rev | cut -c16- | rev)
 
-	gpg --decrypt --output ${output} "${1}" &&
-		echo "$(basename "${1}") -> $(basename "${output}")"
+    gpg --decrypt --output ${output} "${1}" &&
+        echo "$(basename "${1}") -> $(basename "${output}")"
 }
 
 function create-env {
-	# Check that number of arguments is 1
-	if [ $# -ne 2 ]; then
-		echo "Please input the name of your environment and the python version to use as the arguments."
-		return 1
-	fi
+    # Check that number of arguments is 1
+    if [ $# -ne 2 ]; then
+        echo "Please input the name of your environment and the python version to use as the arguments."
+        return 1
+    fi
 
-	# Store the name as a variable
-	name=$1
-	py_version=$2
+    # Store the name as a variable
+    name=$1
+    py_version=$2
 
-	echo "Creating environment the environment \"$name\" will overwrite any other environment with the same name"
-	echo "Are you sure you want to create the environment \"$name\" (y/n)?"
-	read choice
-	if [ "$choice" = "y" ]; then
-		echo "Starting creation"
-	elif [ "$choice" = "n" ]; then
-		echo "Aborting"
-		return 0
-	else
-		echo "Invalid selection"
-		return 1
-	fi
+    echo "Creating environment the environment \"$name\" will overwrite any other environment with the same name"
+    echo "Are you sure you want to create the environment \"$name\" (y/n)?"
+    read choice
+    if [ "$choice" = "y" ]; then
+        echo "Starting creation"
+    elif [ "$choice" = "n" ]; then
+        echo "Aborting"
+        return 0
+    else
+        echo "Invalid selection"
+        return 1
+    fi
 
-	# Create the environment
-	conda create -q -y -n $name python=$py_version pip ipykernel &>/dev/null &&
-		echo "Created environment \"$name\"" || {
-		echo "Environment creation failed"
-		return 1
-	}
+    # Create the environment
+    conda create -q -y -n $name python=$py_version pip ipykernel &>/dev/null &&
+        echo "Created environment \"$name\"" || {
+        echo "Environment creation failed"
+        return 1
+    }
 
-	# Activate the environment
-	conda activate $name &>/dev/null &&
-		echo "Activated environment \"$name\"" || {
-		echo "Environment activation failed"
-		return 1
-	}
+    # Activate the environment
+    conda activate $name &>/dev/null &&
+        echo "Activated environment \"$name\"" || {
+        echo "Environment activation failed"
+        return 1
+    }
 
-	# Create the ipykernel
-	python -m ipykernel install --user --name $name &>/dev/null &&
-		echo "Created ipykernel \"$name\"" || {
-		echo "ipykernel creation failed"
-		return 1
-	}
+    # Create the ipykernel
+    python -m ipykernel install --user --name $name &>/dev/null &&
+        echo "Created ipykernel \"$name\"" || {
+        echo "ipykernel creation failed"
+        return 1
+    }
 }
 
 function remove-env {
-	# Check that number of arguments is 1
-	if [ $# -ne 1 ]; then
-		echo "Please input the name of your environment as the argument."
-		return 1
-	fi
+    # Check that number of arguments is 1
+    if [ $# -ne 1 ]; then
+        echo "Please input the name of your environment as the argument."
+        return 1
+    fi
 
-	# Store the name as a variable
-	name=$1
+    # Store the name as a variable
+    name=$1
 
-	echo "Are you sure you want to delete this environment (y/n)?"
-	read choice
-	if [ "$choice" = "y" ]; then
-		echo "Starting removal"
-	elif [ "$choice" = "n" ]; then
-		echo "Aborting"
-		return 0
-	else
-		echo "Invalid selection"
-		return 1
-	fi
+    echo "Are you sure you want to delete this environment (y/n)?"
+    read choice
+    if [ "$choice" = "y" ]; then
+        echo "Starting removal"
+    elif [ "$choice" = "n" ]; then
+        echo "Aborting"
+        return 0
+    else
+        echo "Invalid selection"
+        return 1
+    fi
 
-	# Uninstall the ipykernel
-	jupyter kernelspec uninstall -y $name &>/dev/null &&
-		echo "Remove ipykernel \"$name\"" || {
-		echo "ipykernel \"$name\" deletion failed"
-		return 1
-	}
+    # Uninstall the ipykernel
+    jupyter kernelspec uninstall -y $name &>/dev/null &&
+        echo "Remove ipykernel \"$name\"" || {
+        echo "ipykernel \"$name\" deletion failed"
+        return 1
+    }
 
-	# Remove the environment
-	conda remove -q -y -n $name --all &>/dev/null &&
-		echo "Removed environment \"$name\"" || {
-		echo "Environment removal failed"
-		return 1
-	}
+    # Remove the environment
+    conda remove -q -y -n $name --all &>/dev/null &&
+        echo "Removed environment \"$name\"" || {
+        echo "Environment removal failed"
+        return 1
+    }
 }
 
 function rcd {
-	# Check that number of arguments ie 1
-	if [ $# -ne 1 ]; then
-		echo "Please input the path to a directory as the argument."
-		return 1
-	fi
+    # Check that number of arguments ie 1
+    if [ $# -ne 1 ]; then
+        echo "Please input the path to a directory as the argument."
+        return 1
+    fi
 
-	# Store the given path as a variable
-	dirpath=$1
+    # Store the given path as a variable
+    dirpath=$1
 
-	# Check that the path is a directory
-	if [ ! -d $dirpath ]; then
-		echo "Please input the path to a directory as the argument."
-		return 1
-	fi
+    # Check that the path is a directory
+    if [ ! -d $dirpath ]; then
+        echo "Please input the path to a directory as the argument."
+        return 1
+    fi
 
-	# While $dirpath contains a single item...
-	while [ $(ls $dirpath | wc -l) -eq 1 ]; do
-		# If the item in the directory is also a directory
-		if [ -d "$dirpath/$(ls $dirpath)" ]; then
-			# Add it to the path to cd into
-			dirpath=$dirpath/$(ls $dirpath)
-		else
-			break
-		fi
-	done
+    # While $dirpath contains a single item...
+    while [ $(ls $dirpath | wc -l) -eq 1 ]; do
+        # If the item in the directory is also a directory
+        if [ -d "$dirpath/$(ls $dirpath)" ]; then
+            # Add it to the path to cd into
+            dirpath=$dirpath/$(ls $dirpath)
+        else
+            break
+        fi
+    done
 
-	# cd into the directories
-	cd $dirpath
+    # cd into the directories
+    cd $dirpath
 }
 
 function wdiff {
-	input=$1
-	output=$2
+    input=$1
+    output=$2
 
-	# Check that the paths is a file
-	if [ ! -f $input ]; then
-		echo "Please input the path to a file as the first argument."
-		return 1
-	elif [ ! -f $output ]; then
-		echo "Please input the path to a file as the second argument."
-		return 1
-	fi
+    # Check that the paths is a file
+    if [ ! -f $input ]; then
+        echo "Please input the path to a file as the first argument."
+        return 1
+    elif [ ! -f $output ]; then
+        echo "Please input the path to a file as the second argument."
+        return 1
+    fi
 
-	git diff -U$(wc -l $input | awk "{print $1}") --word-diff --no-index --no-prefix --color -- $input $output | grep -v ^@@ | less -R
+    git diff -U$(wc -l $input | awk "{print $1}") --word-diff --no-index --no-prefix --color -- $input $output | grep -v ^@@ | less -R
 
 }
 
 function e {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		cd "$cwd"
-	fi
-	rm -f -- "$tmp"
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        cd "$cwd"
+    fi
+    rm -f -- "$tmp"
+}
+
+function ntfy {
+    local title="NTFY Notification"
+    local priority="default"
+    local tags="bell"
+    local destination="main"
+    local message=""
+    local i=0
+    local usage_message="Usage:\n\tntfy -m <message> [-t <title>] [-p <priority>] [-g <tags>] [-d <destination>]\n\tntfy <message> [-t <title>] [-p <priority>] [-g <tags>] [-d <destination>]"
+
+    if [ $# -eq 0 ]; then
+        echo "$usage_message"
+        return 1
+    fi
+
+    while [ $# -gt 0 ]; do
+        case $1 in
+        -t | --title)
+            title="$2"
+            shift 2
+            ;;
+        -p | --priority)
+            priority="$2"
+            shift 2
+            ;;
+        -g | --tags)
+            tags="$2"
+            shift 2
+            ;;
+        -d | --destination)
+            destination="$2"
+            shift 2
+            ;;
+        -m | --message)
+            message="$2"
+            shift 2
+            ;;
+        *)
+            if [ $i -eq 0 ]; then
+                message="$1"
+                shift 1
+            else
+                echo "$usage_message"
+                return 1
+            fi
+            ;;
+        esac
+        i=$((i + 1))
+    done
+
+    # Check if message is provided
+    if [ -z "$message" ]; then
+        echo "Error: Message is required."
+        echo "$usage_message"
+        return 1
+    fi
+
+    # Perform the curl command with the provided and default values
+    curl \
+        -L \
+        -H "Title: ${title}" \
+        -H "Priority: ${priority}" \
+        -H "Tags: ${tags}" \
+        -H "Markdown: true" \
+        -d "${message}" \
+        "https://ntfy.home.etiennecollin.com/${destination}"
+}
 }
--- dot_config/zsh/os/macos/functions.zsh
+++ dot_config/zsh/os/macos/functions.zsh
@@ -5,174 +5,174 @@ function udp {
 
 alias o="custom-open"
 function custom-open {
-	input_paths="$@"
+    input_paths="$@"
 
-	# Check that number of arguments is 1
-	if [ -z "$input_paths" ]; then
-		open .
-		return 1
-	fi
+    # Check that number of arguments is 1
+    if [ -z "$input_paths" ]; then
+        open .
+        return 1
+    fi
 
-	# Open each path
-	for input_path in $input_paths; do
-		# Get the basename and extension of the file
-		full_name="$(basename $input_path)"
-		extension="${full_name##*.}"
+    # Open each path
+    for input_path in $input_paths; do
+        # Get the basename and extension of the file
+        full_name="$(basename $input_path)"
+        extension="${full_name##*.}"
 
-		# Check that the path exists
-		if [ ! -e "$input_path" ]; then
-			echo "$full_name does not exist"
-			continue
-		fi
+        # Check that the path exists
+        if [ ! -e "$input_path" ]; then
+            echo "$full_name does not exist"
+            continue
+        fi
 
-		if [ "$extension" = "pdf" ] && [ $(command -v "sioyek") ] &>/dev/null; then
-			sioyek "$input_path" &>/dev/null &
-		else
-			open "$input_path"
-		fi
-	done
+        if [ "$extension" = "pdf" ] && [ $(command -v "sioyek") ] &>/dev/null; then
+            sioyek "$input_path" &>/dev/null &
+        else
+            open "$input_path"
+        fi
+    done
 }
 
 function cleaks {
-	# Get arguments
-	files="$@"
+    # Get arguments
+    files="$@"
 
-	# Check that there are arguments
-	if [ -z "$files" ]; then
-		echo "Please input the .c files as arguments."
-		echo "The first argument should be the main file."
-		return 1
-	fi
+    # Check that there are arguments
+    if [ -z "$files" ]; then
+        echo "Please input the .c files as arguments."
+        echo "The first argument should be the main file."
+        return 1
+    fi
 
-	# Verify that all the arguments are files with a .c extension
-	for file in $files; do
-		if [ ! -f "$file" ] || [ "${file##*.}" != "c" ]; then
-			echo "Please only input existing files with a .c extension."
-			return 1
-		fi
-	done
+    # Verify that all the arguments are files with a .c extension
+    for file in $files; do
+        if [ ! -f "$file" ] || [ "${file##*.}" != "c" ]; then
+            echo "Please only input existing files with a .c extension."
+            return 1
+        fi
+    done
 
-	# Create the output directory if it does not exist
-	mkdir -p analysis
+    # Create the output directory if it does not exist
+    mkdir -p analysis
 
-	# Get the basename of the first file
-	filename="$(basename $1)"
-	filename="${filename%.*}"
+    # Get the basename of the first file
+    filename="$(basename $1)"
+    filename="${filename%.*}"
 
-	# Compile the .c files
-	clang -Wall -O0 -g -o "analysis/$filename" $files &>analysis/leaks_output.txt
-	cd analysis
+    # Compile the .c files
+    clang -Wall -O0 -g -o "analysis/$filename" $files &>analysis/leaks_output.txt
+    cd analysis
 
-	# Make the process debuggable
-	# Check if the tmp.entitlements file exists
-	if [ ! -f "tmp.entitlements" ]; then
-		/usr/libexec/PlistBuddy -c "Add :com.apple.security.get-task-allow bool true" tmp.entitlements &>/dev/null
-	fi
-	codesign -s - --entitlements tmp.entitlements -f "$filename" &>/dev/null
+    # Make the process debuggable
+    # Check if the tmp.entitlements file exists
+    if [ ! -f "tmp.entitlements" ]; then
+        /usr/libexec/PlistBuddy -c "Add :com.apple.security.get-task-allow bool true" tmp.entitlements &>/dev/null
+    fi
+    codesign -s - --entitlements tmp.entitlements -f "$filename" &>/dev/null
 
-	# Start malloc logging
-	export MallocStackLogging=1
-	export MallocScribble=1
+    # Start malloc logging
+    export MallocStackLogging=1
+    export MallocScribble=1
 
-	# Run leaks on the executable
-	leaks -quiet -groupByType -conservative -atExit -- "./$filename" &>>leaks_output.txt
+    # Run leaks on the executable
+    leaks -quiet -groupByType -conservative -atExit -- "./$filename" &>>leaks_output.txt
 
-	# Stop malloc logging
-	unset MallocStackLogging
-	unset MallocScribble
+    # Stop malloc logging
+    unset MallocStackLogging
+    unset MallocScribble
 
-	# Print the output
-	cd ..
-	echo "Leaks output -> analysis/leaks_output.txt"
+    # Print the output
+    cd ..
+    echo "Leaks output -> analysis/leaks_output.txt"
 }
 
 function canalysis {
-	########################
-	# To be used with a Docker image
-	# Dockerfile contents:
-	#     FROM alpine:latest
-	#     RUN apk add clang valgrind compiler-rt llvm17
-	#
-	# Create the image:
-	#     docker build -t "canalysis" .
-	#
-	# To delete the image:
-	#     docker rmi -f canalysis
-	########################
+    ########################
+    # To be used with a Docker image
+    # Dockerfile contents:
+    #     FROM alpine:latest
+    #     RUN apk add clang valgrind compiler-rt llvm17
+    #
+    # Create the image:
+    #     docker build -t "canalysis" .
+    #
+    # To delete the image:
+    #     docker rmi -f canalysis
+    ########################
 
-	# Get arguments
-	files="$@"
+    # Get arguments
+    files="$@"
 
-	# Check that there are arguments
-	if [ -z "$files" ]; then
-		echo "Please input the .c files as arguments."
-		echo "The first argument should be the main file."
-		return 1
-	fi
+    # Check that there are arguments
+    if [ -z "$files" ]; then
+        echo "Please input the .c files as arguments."
+        echo "The first argument should be the main file."
+        return 1
+    fi
 
-	# Verify that all the arguments are files with a .c extension
-	for file in $files; do
-		if [ ! -f "$file" ] || [ "${file##*.}" != "c" ]; then
-			echo "Please only input existing files with a .c extension."
-			return 1
-		fi
-	done
+    # Verify that all the arguments are files with a .c extension
+    for file in $files; do
+        if [ ! -f "$file" ] || [ "${file##*.}" != "c" ]; then
+            echo "Please only input existing files with a .c extension."
+            return 1
+        fi
+    done
 
-	# Verify that the Docker daemon is running
-	if [ ! "$(docker stats --no-stream 2>/dev/null)" ]; then
-		echo "Please start the Docker daemon. Either run the Docker application or start it manually."
-		return 1
-	fi
+    # Verify that the Docker daemon is running
+    if [ ! "$(docker stats --no-stream 2>/dev/null)" ]; then
+        echo "Please start the Docker daemon. Either run the Docker application or start it manually."
+        return 1
+    fi
 
-	# Verify that the Docker image exists
-	if [ "$(docker image inspect -f {{.Os}} canalysis)" != "linux" ]; then
-		echo "Docker image does not exist."
-		echo "Please create it with the following command:"
-		echo "> echo \"FROM alpine:latest\\\nRUN apk add clang valgrind compiler-rt llvm17\\\n\" > Dockerfile"
-		echo "Then build it with:"
-		echo "> docker build -t \"canalysis\" ."
-		echo "And, if you need to, delete it with:"
-		echo "> docker rmi -f canalysis"
-		return 1
-	fi
+    # Verify that the Docker image exists
+    if [ "$(docker image inspect -f {{.Os}} canalysis)" != "linux" ]; then
+        echo "Docker image does not exist."
+        echo "Please create it with the following command:"
+        echo "> echo \"FROM alpine:latest\\\nRUN apk add clang valgrind compiler-rt llvm17\\\n\" > Dockerfile"
+        echo "Then build it with:"
+        echo "> docker build -t \"canalysis\" ."
+        echo "And, if you need to, delete it with:"
+        echo "> docker rmi -f canalysis"
+        return 1
+    fi
 
-	# Create the output directory if it does not exist
-	mkdir -p analysis
+    # Create the output directory if it does not exist
+    mkdir -p analysis
 
-	# Get the basename of the first file
-	filename="$(basename $1)"
-	filename="${filename%.*}"
+    # Get the basename of the first file
+    filename="$(basename $1)"
+    filename="${filename%.*}"
 
-	# Command to attach to the Docker image
-	docker_command="docker run -it -v $PWD:/tmp -w /tmp canalysis"
+    # Command to attach to the Docker image
+    docker_command="docker run -it -v $PWD:/tmp -w /tmp canalysis"
 
-	########################
-	# Analyse with the sanitizer
-	########################
-	# Command to compile the .c files
-	compile_command="clang -Wall -O0 -g -fsanitize=address -fno-optimize-sibling-calls -fno-omit-frame-pointer -o analysis/$filename $files"
-	# Command to convert the addresses to symbols
-	symbolize_command="export ASAN_SYMBOLIZER_PATH=\$(which llvm-symbolizer); llvm-symbolizer ./$filename &>/dev/null"
+    ########################
+    # Analyse with the sanitizer
+    ########################
+    # Command to compile the .c files
+    compile_command="clang -Wall -O0 -g -fsanitize=address -fno-optimize-sibling-calls -fno-omit-frame-pointer -o analysis/$filename $files"
+    # Command to convert the addresses to symbols
+    symbolize_command="export ASAN_SYMBOLIZER_PATH=\$(which llvm-symbolizer); llvm-symbolizer ./$filename &>/dev/null"
 
-	# Combine the commands and execute them
-	full_command="/bin/sh -c \"\$compile_command; cd analysis; \$symbolize_command; ./$filename\""
-	eval $docker_command $full_command &>analysis/sanitizer_output.txt
+    # Combine the commands and execute them
+    full_command="/bin/sh -c \"\$compile_command; cd analysis; \$symbolize_command; ./$filename\""
+    eval $docker_command $full_command &>analysis/sanitizer_output.txt
 
-	# Print the output
-	echo "Sanitizer output -> analysis/sanitizer_output.txt"
+    # Print the output
+    echo "Sanitizer output -> analysis/sanitizer_output.txt"
 
-	########################
-	# Analyse with valgrind
-	########################
-	# Command to compile the .c files
-	compile_command="clang -Wall -O0 -g -o analysis/$filename $files"
-	# Command to run valgrind on the executable
-	valgrind_command="valgrind -s --leak-check=full --leak-resolution=med --track-origins=yes --track-fds=yes --trace-children=yes --show-leak-kinds=all --vgdb=no ./$filename"
+    ########################
+    # Analyse with valgrind
+    ########################
+    # Command to compile the .c files
+    compile_command="clang -Wall -O0 -g -o analysis/$filename $files"
+    # Command to run valgrind on the executable
+    valgrind_command="valgrind -s --leak-check=full --leak-resolution=med --track-origins=yes --track-fds=yes --trace-children=yes --show-leak-kinds=all --vgdb=no ./$filename"
 
-	# Combine the commands and execute them
-	full_command="/bin/sh -c \"\$compile_command; cd analysis; \$valgrind_command\""
-	eval $docker_command $full_command &>analysis/valgrind_output.txt
+    # Combine the commands and execute them
+    full_command="/bin/sh -c \"\$compile_command; cd analysis; \$valgrind_command\""
+    eval $docker_command $full_command &>analysis/valgrind_output.txt
 
-	# Print the output
-	echo "Valgrind output -> analysis/valgrind_output.txt"
+    # Print the output
+    echo "Valgrind output -> analysis/valgrind_output.txt"
 }
