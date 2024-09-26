a779d5cb07cb0d7d5f0b33ac738239fd73b54cb5
etiennecollin <collin.etienne.contact@gmail.com>
2024-09-26 11:09:16 -0400

    Added pipx to path

--- dot_config/zsh/os/macos/env.zsh
+++ dot_config/zsh/os/macos/env.zsh
@@ -1,14 +1,17 @@
 export XDG_CONFIG_HOME="$HOME/.config"
 
+# Export pipx path
+export PATH="$PATH:$HOME/.local/bin"
+
 # Initialize Cargo
 source "$HOME/.cargo/env"
 
 # Set PATH, MANPATH, etc., for Homebrew.
 if [ -f "/opt/homebrew/bin/brew" ]; then
-	eval "$(/opt/homebrew/bin/brew shellenv)"
-	FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-	# Use Homebrew's Python
-	export PATH="$(brew --prefix python)/libexec/bin:$PATH"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+    # Use Homebrew's Python
+    export PATH="$(brew --prefix python)/libexec/bin:$PATH"
 fi
 
 # Initialize Ruby downloaded via Homebrew's chruby
@@ -17,4 +20,4 @@ source /opt/homebrew/opt/chruby/share/chruby/auto.sh
 chruby ruby-3.1.3 # run 'chruby' to see actual version to se as argument
 
 # Initialize Haskell
-[ -f "/Users/etiennecollin/.ghcup/env" ] && source "/Users/etiennecollin/.ghcup/env" # ghcup-env
+[ -f "$HOME/.ghcup/env" ] && source "$HOME/.ghcup/env" # ghcup-env
