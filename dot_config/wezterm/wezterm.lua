cfc81139c6995e0b8972a1e22bdb0b218d230ffd
etiennecollin <collin.etienne.contact@gmail.com>
2024-10-31 16:22:32 -0400

    Updated terminal configs

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,36 +7,32 @@ end
 
 -- Configure looks
 config.color_scheme = "Gruvbox dark, medium (base16)"
+
 config.font = wezterm.font("Maple Mono NF", { weight = "Regular" })
-config.harfbuzz_features = { "zero", "cv01", "cv02", "ss03" }
 config.font_size = 16
+config.harfbuzz_features = { "zero", "cv01", "cv02", "ss03" }
 
--- Configure window opacity
-config.window_background_opacity = 1
+config.adjust_window_size_when_changing_font_size = false
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.cursor_blink_rate = 400
+config.enable_scroll_bar = true
+config.max_fps = 120
 
--- Configure image background
--- config.text_background_opacity = 1
--- config.window_background_image = "/Users/etiennecollin/github/dotfiles/wallpapers/ancient_bristlecone_pine_forest.jpg"
--- config.window_background_image_hsb = {
--- 	brightness = 0.8,
--- }
-
-config.inactive_pane_hsb = {
-	saturation = 0.8,
-	brightness = 0.7,
-}
-
--- Config for window
+-- Config for windows
 config.hide_tab_bar_if_only_one_tab = true
-config.window_frame = {
-	font_size = 16,
-}
+config.inactive_pane_hsb = { saturation = 0.8, brightness = 0.7 }
+
+config.window_background_opacity = 1
 config.window_decorations = "RESIZE"
+config.window_frame = { font_size = 16 }
 config.window_padding = { left = 10, right = 10, top = 10, bottom = 0 }
 
 -- Other settings
 config.use_dead_keys = true
-config.enable_scroll_bar = true
-config.adjust_window_size_when_changing_font_size = false
+config.enable_kitty_graphics = true
+config.front_end = "WebGpu"
+config.webgpu_power_preference = "LowPower"
+-- config.term = "xterm-kitty"
 
 return config
