d8c057d729a366b95f0c1548071636cb644c0197
ecollin <etienne.collin@reactiondynamics.space>
2025-10-24 17:40:46 -0400

    Updated config

---
+++ dot_config/zsh/os/debian/env.zsh
@@ -0,0 +1,33 @@
+# NNN
+export LC_COLLATE="C" # hidden files on top
+export NNN_PLUG="p:preview-tui"
+export NNN_FIFO="/tmp/nnn.fifo"
+export SPLIT='v'                              # to split Kitty vertically
+export NNN_FCOLORS="AAAAE631BBBBCCCCDDDD9999" # feel free to change the colors
+
+# XDG-ninja
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+
+export XDG_DESKTOP_DIR="$HOME/Desktop"
+export XDG_DOCUMENTS_DIR="$HOME/Documents"
+export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+export XDG_MUSIC_DIR="$HOME/Music"
+export XDG_PICTURES_DIR="$HOME/Pictures"
+export XDG_PUBLICSHARE_DIR="$HOME/Public"
+export XDG_TEMPLATES_DIR="$HOME/Templates"
+export XDG_VIDEOS_DIR="$HOME/Videos"
+
+export HISTFILE="$XDG_STATE_HOME"/zsh/history
+export GNUPGHOME="$XDG_DATA_HOME"/gnupg
+export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
+export XCURSOR_PATH=/usr/share/icons:${XDG_DATA_HOME}/icons
+export XAUTHORITY="$XDG_RUNTIME_DIR"/Xauthority
+export CUDA_CACHE_PATH="$XDG_CACHE_HOME"/nv
+
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
+
+export PATH="$HOME/.cargo/bin:$PATH"
--- dot_config/zsh/plugins.zsh
+++ dot_config/zsh/plugins.zsh
@@ -1,8 +1,8 @@
 # Download zinit, if it's not there yet
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 if [ ! -d "$ZINIT_HOME" ]; then
-	mkdir -p "$(dirname $ZINIT_HOME)"
-	git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+    mkdir -p "$(dirname $ZINIT_HOME)"
+    git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 fi
 
 # Source/Load zinit
