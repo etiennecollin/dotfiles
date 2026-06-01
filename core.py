0fdf0d4300c39d01da314fe7aabcb18399fcd346
etiennecollin <collin.etienne.contact@gmail.com>
2026-06-01 15:05:00 -0400

    feat: add Sunshine

--- core.py
+++ core.py
@@ -254,6 +254,33 @@ dnf.packages(
 )
 
 # ==============================================================================
+# Sunshine
+# ==============================================================================
+
+copr_list = host.get_fact(Command, "dnf copr list")
+server.shell(
+    name="Enable COPR lizardbyte/stable",
+    commands=["yes | dnf copr enable lizardbyte/stable"],
+    _sudo=True,
+    _if=lambda: "lizardbyte/stable" not in copr_list,
+)
+
+dnf.packages(
+    name="Install Sunshine",
+    packages=["Sunshine"],
+    latest=True,
+    _sudo=True,
+)
+
+systemd.service(
+    name="Enable `Sunshine` user service",
+    service="app-dev.lizardbyte.app.Sunshine",
+    running=True,
+    enabled=True,
+    user_mode=True,
+)
+
+# ==============================================================================
 # Go packages
 # ==============================================================================
 
